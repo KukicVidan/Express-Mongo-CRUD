b50d11eacb5bd98d7566bb5146e50b682536900b
Vidan Kukic <kukicvidan@gmail.com>
2024-12-30 02:05:53 +0100

    add styles folder and css for index page

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(express.static('public')); // Serve static files
 
 
 
